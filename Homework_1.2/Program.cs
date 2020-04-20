5b6c72a382d458606f02408b2200cab9080015ba
Lemeschkin <Lemeschkin@gmail.com>
2020-04-20 22:25:38 +0300

    Adding task_2 files

---
+++ Homework_1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter to string");
+            string str = Console.ReadLine();
+            string str1 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 1)
+                str1 = string.Concat(str1, str[i]);
+            }
+            Console.WriteLine("New string:");
+            Console.WriteLine(str1);
+        }
+    }
+}
