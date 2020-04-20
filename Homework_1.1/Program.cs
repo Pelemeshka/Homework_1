e5668e65d68ca2d3cd8d81c1757938e8523fa1cf
Lemeschkin <Lemeschkin@gmail.com>
2020-04-20 22:24:50 +0300

    Adding task_1 files

---
+++ Homework_1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements in the array:");
+            int n = Convert.ToInt16(Console.ReadLine());
+            int[] M = new int[n];
+            Random r = new Random();
+            Console.WriteLine("Source array:");
+            int sum = 0;
+            for (int k = 0; k < n; k++)
+            {
+                M[k] = r.Next(10);
+                Console.Write(M[k] + "  ");
+                if (M[k] % 2 == 0) sum =  M[k] + sum;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum even elements of array: " + sum);
+
+        }
+
+    }
+}
